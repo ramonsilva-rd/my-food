eeb5eff96d6fe49fe1ed55852e10af78ba81dc78
Ramon Ferreira Silva <ramon.silva@resultadosdigitais.com.br>
2021-03-24 22:26:36 -0300

    :lock: add user auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 
   def index
-    render 'Hello world'
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :restaurants, dependent: :destroy
+  has_many :meals, dependent: :destroy
 end
